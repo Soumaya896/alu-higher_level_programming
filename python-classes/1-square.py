a695bb5a271d3656a61fc907113262457b9484a4
Soumaya896 <t.soumaya@alustudent.com>
2026-07-29 04:15:37 -0700

    Fix file content for 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
-nano 2-square.py
-nano 3-square.py
-nano 4-square.py
-nano 5-square.py
-nano 6-square.py
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
