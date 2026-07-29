9eee27a1df3944cce23e339cf65da788a8e0feef
Soumaya896 <t.soumaya@alustudent.com>
2026-07-29 04:03:02 -0700

    Complete OOP python-classes project tasks 0 through 6

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a Square class that can print itself."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using the # character."""
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
