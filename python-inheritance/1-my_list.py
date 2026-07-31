95a13cb907ad633484e958d5a9f0b77733cace9a
Soumaya896 <t.soumaya@alustudent.com>
2026-07-31 11:07:56 -0700

    Fix PEP8 line length in 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,6 +11,6 @@ class MyList(list):
 
     def print_sorted(self):
         """
-        Prints the list in ascending sorted order without modifying the original.
+        Prints the list in ascending sorted order.
         """
         print(sorted(self))
